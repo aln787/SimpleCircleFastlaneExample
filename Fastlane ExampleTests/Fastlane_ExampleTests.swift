eaa8facb78b9f8fc6a458884b94ef5ae4073c400
aln787 <aniderberg@gmail.com>
2016-06-04 14:21:30 -0400

    Removing the space from the project's name

--- Fastlane ExampleTests/Fastlane_ExampleTests.swift
+++ Fastlane ExampleTests/Fastlane_ExampleTests.swift
@@ -7,30 +7,26 @@
 //
 
 import XCTest
-@testable import Fastlane_Example
+//@testable import FastlaneExample
 
 class Fastlane_ExampleTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
+    func testWhiteListLogic() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let test = LoginController(user: "Alex")
+        XCTAssertFalse(test.userOnWhiteList())
+        
+        let test2 = LoginController(user: "Woz")
+        XCTAssertTrue(test2.userOnWhiteList())
+        
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
--- Fastlane ExampleUITests/Fastlane_ExampleUITests.swift
+++ Fastlane ExampleUITests/Fastlane_ExampleUITests.swift
@@ -28,9 +28,7 @@ class Fastlane_ExampleUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
     
 }

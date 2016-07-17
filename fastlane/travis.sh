d3b11027e222f3c59496dc571beb5762edf99412
Alex Niderberg <aniderberg@gmail.com>
2016-07-17 09:31:10 -0400

    Correcting the add path command for Travis

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 bundle install
 echo $TRAVIS
-if [ -z $TRAVIS ]; then export PATH="./vendor/bundle:$PATH"
+if [ -z $TRAVIS ]; then export PATH="./vendor/bundle:$PATH"; fi
 fastlane ios test
 exit $?
 
